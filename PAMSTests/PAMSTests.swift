033b91fd96448056dbd0b6b0f70a71f2cbbebcb4
Leonardo <158120970+leonardonapoless@users.noreply.github.com>
2025-10-23 07:00:56 -0300

    Initial Commit

---
+++ PAMSTests/PAMSTests.swift
@@ -0,0 +1,17 @@
+//
+//  PAMSTests.swift
+//  PAMSTests
+//
+//  Created by Leonardo Nápoles on 10/23/25.
+//
+
+import Testing
+@testable import PAMS
+
+struct PAMSTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
